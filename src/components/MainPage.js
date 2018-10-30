a8cbcd8646238813d8cf9e8857ca07279d89c447
Roland Abiog <rtabiog@yahoo.com>
2018-10-30 02:28:17 -0500

    Break-up App.js and create the components

---
+++ src/components/MainPage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import BookShelf from './BookShelf';
+
+class MainPage extends React.Component {
+  state = {}
+
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <BookShelf/>
+          </div>
+        </div>
+        <div className="open-search">
+          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MainPage;
+    
