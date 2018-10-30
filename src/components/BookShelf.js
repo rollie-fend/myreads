dd28211af8614afc2ca64e8dbf8c0ea87ae9730a
Roland Abiog <rtabiog@yahoo.com>
2018-10-30 02:30:19 -0500

    Connect to data source through API, get all book data, replace hard-coded data

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -2,13 +2,18 @@ import React from 'react'
 import Book from './Book'
 
 class BookShelf extends React.Component {
+  componentDidMount() {
+    console.log(this);
+  }
   render() {
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
+        <h2 className="bookshelf-title">{this.props.title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <Book/>
+           {
+            this.props.books.map((book,key)=> <Book book={book} key={book.id} />)
+          }
           </ol>
         </div>
       </div>
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,25 +1,42 @@
 import React from 'react'
+import * as BooksAPI from '../BooksAPI'
 import BookShelf from './BookShelf';
 
 class MainPage extends React.Component {
-  state = {}
+/* initialize the state */
+  state = {books:[]}
+
+/* Fetch data from the backend and update the state (populate book array) */
+  componentDidMount() {
+    BooksAPI.getAll()
+    .then(response=> {
+      this.setState({books:response});
+      console.log(response);
+    })
+  }
 
   render() {
     return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            <BookShelf/>
+      <div className="app">
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <div className="bookshelf">
+                  <BookShelf title="Currently Reading" books={this.state.books.filter(b=>b.shelf==="currentlyReading")}/>
+                  <BookShelf title="Want to Read" books={this.state.books.filter(b=>b.shelf==="wantToRead")}/>
+                  <BookShelf title="Read" books={this.state.books.filter(b=>b.shelf==="read")}/>
+                </div>
+              </div>
+            </div>
+            <div className="open-search">
+              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+            </div>
           </div>
-        </div>
-        <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-        </div>
       </div>
-    )
+    );
   }
 }
 
